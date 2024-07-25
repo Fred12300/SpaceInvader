d2a92dd13987f8e70a276095cbdef6e9fe1aaf37
Fred12300 <fredcharpentier2@gmail.com>
2024-07-25 16:58:14 +0200

    missile

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
-let enemyVPos = -100;
-setInterval(()=>{
-    vaisseau.clearRect(0, 0, canvas.width, canvas.height);
-    enemyVPos=enemyVPos+1;
-    vaisseau.fillRect( 100, enemyVPos, 100 , 100 )
-    if(enemyVPos>600){
-        enemyVPos = -100;
-    }
-})
-const canvas = document.querySelector("canvas");
-const vaisseau = canvas.getContext("2d");
-vaisseau.fillStyle = "blue";
+let playerPos = document.querySelector(".player")
+let horPlayer = 300;
+let keyPress = (e) => {
+console.log(e.code);
+switch (e.code) {
+    case "Space" :
+        let miss = document.createElement("div");
+        miss.style = `
+        position: relative;
+        height: 50px;
+        width: 20px;
+        background-color: black;
+        top : -60px;`
+        playerPos.appendChild(miss)
+        let vPos = -10;
+        let vPosMin = -550;
+        let vPosSpeed = 5;
+        setInterval(()=>{
+        vPos = vPos - vPosSpeed;
+            if(vPos<vPosMin){
+                miss.remove()
+                return
+            }
+        console.log(vPos);
+        miss.style.top = `${vPos}px`;
+        } )
+    break;
+    case "ArrowLeft" :
+        horPlayer -= 15;
+        playerPos.style.left = `${horPlayer}px`
+    break;
+    case "ArrowRight" :
+        horPlayer += 15;
+        playerPos.style.left = `${horPlayer}px`
+    break;
+}
+}
 
+document.addEventListener("keydown", keyPress)
 
 
